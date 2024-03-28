6bf933b7c7d0f8344faa7d7c4d496460c809fa3b
RobinAlexis <ts1sio@SIO301-10.sio.bts>
2024-03-28 10:20:25 +0100

    Changement nom de la class MetierFonction -> Fonction

---
+++ src/main/java/model/Fonction.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class Fonction {
+    
+    private int id;
+    private String libelle ;
+
+    public Fonction() {
+    }
+
+    public Fonction(int id, String libelle) {
+        this.id = id;
+        this.libelle = libelle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+    
+    
+}
