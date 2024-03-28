66186ee1a51ace1166f52cfe7231c4b022e62208
MAHI-MOUSSA Issam <ts1sio@SIO301-12.sio.bts>
2024-03-28 10:59:10 +0100

    Rename de metierGrade en grade

---
+++ src/main/java/model/Grade.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ts1sio
+ */
+public class Grade {
+    private int id;
+    private String nom ;
+
+    public Grade() {
+    }
+
+    public Grade(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+}
