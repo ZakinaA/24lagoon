d0837d029cc2991fbecc391904cdd0c7642c5bd9
alexi <alexi@DESKTOP-MOS8DPD.home>
2024-04-02 16:40:52 +0200

    Test de la methode getLesPompiersGradeById

--- src/main/java/test/TestDaoGrade.java
+++ src/main/java/test/TestDaoGrade.java
@@ -9,6 +9,7 @@ import database.DaoGrade;
 import java.sql.Connection;
 import java.util.ArrayList;
 import model.Grade;
+import model.Pompier;
 
 /**
  *
@@ -24,6 +25,12 @@ public class TestDaoGrade {
             System.out.println ("nombre de grades : " + DaoGrade.getLesGrades(cnx).size());
             for (Grade g : lesGrades){
             System.out.println(" . "+ g.getId() + " " + g.getLibelle() + " " + g.getUnSurgrade().getLibelle());
-        }
-        }
+            }
+            
+            ArrayList<Pompier> pompiers = DaoGrade.getLesPompiersGradeById(cnx, 2);
+            System.out.println ("Les pompiers du grade de l'id 1 sont :");
+            for (Pompier pompier : pompiers) {
+                System.out.println("ID: " + pompier.getId() + ", Nom: " + pompier.getNom() + ", Prénom: " + pompier.getPrenom());
+            }
+    }
 }
